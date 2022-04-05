3c786ec7565192e2ab91a1d269a308ab9b31ec45
Nick <spbkolia@gmail.com>
2022-04-05 18:35:52 +0300

    Initial Commit

---
+++ VkPhotoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkPhotoTest
+//
+//  Created by Чаусов Николай on 05.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
